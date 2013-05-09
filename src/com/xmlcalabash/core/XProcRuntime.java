6b28988f9d45342a787c3583f6f4d198888a4870
James Leigh <james@3roundstones.com>
2013-05-09 13:17:23 -0400

    Share a common DataStore through XProcRuntime

--- src/com/xmlcalabash/core/XProcRuntime.java
+++ src/com/xmlcalabash/core/XProcRuntime.java
@@ -80,8 +80,12 @@ import com.xmlcalabash.functions.ValueAvailable;
 import com.xmlcalabash.functions.VersionAvailable;
 import com.xmlcalabash.functions.XPathVersionAvailable;
 import com.xmlcalabash.functions.XProcExtensionFunctionDefinition;
+import com.xmlcalabash.io.DataStore;
+import com.xmlcalabash.io.FileDataStore;
+import com.xmlcalabash.io.HttpClientDataStore;
 import com.xmlcalabash.io.ReadableData;
 import com.xmlcalabash.io.ReadablePipe;
+import com.xmlcalabash.io.URLDataStore;
 import com.xmlcalabash.model.DeclareStep;
 import com.xmlcalabash.model.Parser;
 import com.xmlcalabash.model.PipelineLibrary;
@@ -129,6 +133,7 @@ public class XProcRuntime {
     private XLibrary xStandardLibrary = null;
     private HttpClient httpClient;
     private Map<String, CookieStore> cookieStores;
+    private DataStore dataStore;
     private XProcConfigurer configurer = null;
     private String htmlParser = null;
     private Vector<XProcExtensionFunctionDefinition> exFuncs = new Vector<XProcExtensionFunctionDefinition>();
@@ -339,6 +344,21 @@ public class XProcRuntime {
         uriResolver.setUnderlyingEntityResolver(resolver);
     }
 
+    public synchronized DataStore getDataStore() {
+        if (dataStore == null) {
+            DataStore fallback = new URLDataStore();
+            if (!getSafeMode()) {
+                fallback = new FileDataStore(fallback);
+            }
+            dataStore = new HttpClientDataStore(getHttpClient(), fallback);
+        }
+        return dataStore;
+    }
+
+    public synchronized void setDataStore(DataStore dataStore) {
+        this.dataStore = dataStore;
+    }
+
     public XProcURIResolver getResolver() {
         return uriResolver;
     }
@@ -488,6 +508,7 @@ public class XProcRuntime {
         // Provide custom retry handler is necessary
         httpClient.setHttpRequestRetryHandler(new StandardHttpRequestRetryHandler(3, false));
         this.httpClient = httpClient;
+        dataStore = null;
 
         xprocData = new XProcData(this);
 
