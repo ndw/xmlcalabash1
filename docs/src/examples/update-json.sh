76b27618918cf27de14c676ec7355e4d539e3de6
Norman Walsh <ndw@nwalsh.com>
2013-07-28 14:28:01 -0400

    Ported changes to the docs and docs build process from saxon95-dev

--- docs/src/examples/update-json.sh
+++
@@ -1,5 +0,0 @@
-for flavor in "marklogic" "jsonx" "jxml" "calabash" "calabash-deprecated"
-do
-    echo "Converting JSON to $flavor..."
-    calabash -Xtransparent-json -Xjson-flavor=$flavor -oresult=../results/json-$flavor.xml json.xpl
-done
